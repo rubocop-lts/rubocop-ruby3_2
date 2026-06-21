df788b0ab1acffe05f2d6112f411b0f89d4cdaac
Peter H. Boling <peter.boling@gmail.com>
2026-06-21 14:08:48 -0600

    🔧 Replace deprecated nocov markers

--- lib/rubocop/ruby3_2.rb
+++ lib/rubocop/ruby3_2.rb
@@ -5,9 +5,9 @@ require "version_gem"
 
 # this gem
 require_relative "ruby3_2/version"
-# :nocov:
+# simplecov:disable
 require_relative "ruby3_2/railtie" if defined?(Rails::Railtie)
-# :nocov:
+# simplecov:enable
 
 module Rubocop
   # Namespace of this library
